c04f170b0a80ba89710aa16276cea57ab6f0bb4a
Linn Thit <linnthit77387@gmail.com>
2024-03-16 19:50:40 +0630

    Add project files.

---
+++ OJTBatch1DotNetCore.ConsoleAppDay1/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello!"); // ClassName.MethodName
+        // Console => class name & WriteLine => method (static)
+        // F5
+        string name = "Linn Thit"; // string default value => null or ""
+        bool isActive; // bool default value => false
+        long id = 10; // long, int, decimal, float default value => 0
+        int userID = 100; // default value 0
+        //if (string.IsNullOrEmpty(name))
+        //{
+
+        //}
+        Console.WriteLine(name);
+
+
+        Console.ReadKey();
+    }
+}
