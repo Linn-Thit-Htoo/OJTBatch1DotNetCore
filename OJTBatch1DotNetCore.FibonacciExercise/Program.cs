21eeb81b2f37853070a8f8368fef2f05313206c5
Linn Thit <linnthit77387@gmail.com>
2024-03-24 20:45:33 +0630

    Exercise 1 added

---
+++ OJTBatch1DotNetCore.FibonacciExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        //  0, 1, 1, 2, 3, 5
+        int firstNum = 0;
+        int secondNum = 1;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            int sum = firstNum + secondNum; // 1, 2, 3, 5
+            if (i == 1)
+            {
+                Console.Write($"{firstNum}, {secondNum}, {sum}, "); // 0, 1, 1
+                //Console.Write($"{firstNum}, {secondNum}, {sum} "); // 0, 1, 1
+            }
+            else
+            {
+                Console.Write(i == 10 ? sum : sum + ", ");
+                //Console.Write(", " + sum);
+            }
+            firstNum = secondNum;
+            secondNum = sum;
+        }
+    }
+}
