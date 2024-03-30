614e5ea715fcc007687ecdd0693531da722f7909
Linn Thit <linnthit77387@gmail.com>
2024-03-30 21:06:31 +0630

    -

---
+++ OJTBatch1DotNetCore.BubbleSort/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace OJTBatch1DotNetCore.BubbleSort
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] nums = { 4, 3, 2, 1 };
+
+            bool isSorted = false;
+            while (!isSorted)
+            {
+                isSorted = true;
+                for (int i = 0; i < nums.Length - 1; i++)
+                {
+                    int previousNum = nums[i]; // 4
+                    int nextNum = nums[i + 1]; // 3
+
+                    if (nextNum < previousNum)
+                    {
+                        nums[i] = nextNum;
+                        nums[i + 1] = previousNum;
+                        isSorted = false;
+                    }
+                }
+            }
+
+            foreach (var num in nums)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
