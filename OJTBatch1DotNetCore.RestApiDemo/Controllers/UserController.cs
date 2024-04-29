92a7fd1de66ea30c70f16b7d0d70f78a16982068
Linn Thit <linnthit77387@gmail.com>
2024-04-29 19:35:43 +0630

    Day 21

---
+++ OJTBatch1DotNetCore.RestApiDemo/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OJTBatch1DotNetCore.RestApiDemo.Controllers
+{
+    // /api/User
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Greeting()
+        {
+            return StatusCode(StatusCodes.Status200OK, "Welcome!");
+        }
+    }
+}
