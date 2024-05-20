3e38bc86785d0eeed1789fa6c10b0d739192b680
Linn Thit <lth1212001@gmail.com>
2024-05-20 23:17:18 +0630

    -

--- OJTBatch1DotNetCore.ConsoleAppDay8.Abstraction/Program.cs
+++ OJTBatch1DotNetCore.ConsoleAppDay8.Abstraction/Program.cs
@@ -1,25 +1,24 @@
-﻿namespace OJTBatch1DotNetCore.ConsoleAppDay8.Abstraction
-{
-    public abstract class Animal
-    {
-        public abstract void MakeSound();
-        public void Eat() => Console.WriteLine("Animal is eating..");
-    }
+﻿namespace OJTBatch1DotNetCore.ConsoleAppDay8.Abstraction;
 
-    public class Dog : Animal
+public abstract class Animal
+{
+    public abstract void MakeSound();
+    public void Eat() => Console.WriteLine("Animal is eating..");
+}
+
+public class Dog : Animal
+{
+    public override void MakeSound()
     {
-        public override void MakeSound()
-        {
-            Console.WriteLine("Dog is barking...");
-        }
+        Console.WriteLine("Dog is barking...");
     }
-    public class Program
+}
+public class Program
+{
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            Dog dog = new();
-            dog.MakeSound();
-            dog.Eat();
-        }
+        Dog dog = new();
+        dog.MakeSound();
+        dog.Eat();
     }
 }
