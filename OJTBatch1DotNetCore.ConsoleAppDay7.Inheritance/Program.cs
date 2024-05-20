379c345021f2f2ff436db94cec4cafaa6687c85f
Linn Thit <lth1212001@gmail.com>
2024-05-20 23:16:59 +0630

    -

--- OJTBatch1DotNetCore.ConsoleAppDay7.Inheritance/Program.cs
+++ OJTBatch1DotNetCore.ConsoleAppDay7.Inheritance/Program.cs
@@ -1,27 +1,26 @@
-﻿namespace OJTBatch1DotNetCore.ConsoleAppDay7.Inheritance
-{
-    public class Animal
-    {
-        public string _animalName = "Aung Aung";
-        private int _animalAge;
-        protected string _color = "red";
+﻿namespace OJTBatch1DotNetCore.ConsoleAppDay7.Inheritance;
 
-        public void MakeSound() => Console.WriteLine("Animal is making sound...");
-    }
-    public class Dog : Animal
+public class Animal
+{
+    public string _animalName = "Aung Aung";
+    private int _animalAge;
+    protected string _color = "red";
+
+    public void MakeSound() => Console.WriteLine("Animal is making sound...");
+}
+public class Dog : Animal
+{
+    public void SetColor(string color)
     {
-        public void SetColor(string color)
-        {
-            _color = color;
-        }
+        _color = color;
     }
-    public class Program
+}
+public class Program
+{
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            Dog dog = new();
-            Console.WriteLine(dog._animalName);
-            dog.MakeSound();
-        }
+        Dog dog = new();
+        Console.WriteLine(dog._animalName);
+        dog.MakeSound();
     }
 }
