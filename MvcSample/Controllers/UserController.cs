24c4d4ad79ec79b2550dfa164fb9f568eff51135
Linn Thit <lth1212001@gmail.com>
2024-05-20 23:14:53 +0630

    -

--- MvcSample/Controllers/UserController.cs
+++ MvcSample/Controllers/UserController.cs
@@ -3,64 +3,63 @@ using MvcSample.Models;
 using System.Data;
 using System.Data.SqlClient;
 
-namespace MvcSample.Controllers
+namespace MvcSample.Controllers;
+
+public class UserController : Controller
 {
-    public class UserController : Controller
+    private readonly IConfiguration _configuration;
+
+    public UserController(IConfiguration configuration)
     {
-        private readonly IConfiguration _configuration;
+        _configuration = configuration;
+    }
 
-        public UserController(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
+    public IActionResult Index()
+    {
+        return View();
+    }
 
-        public IActionResult Index()
+    [HttpPost]
+    public IActionResult Login(UserDataModel dataModel)
+    {
+        try
         {
-            return View();
-        }
-
-        [HttpPost]
-        public IActionResult Login(UserDataModel dataModel)
-        {
-            try
+            if (string.IsNullOrEmpty(dataModel.PhoneNumber) || string.IsNullOrEmpty(dataModel.Password))
             {
-                if (string.IsNullOrEmpty(dataModel.PhoneNumber) || string.IsNullOrEmpty(dataModel.Password))
-                {
-                    TempData["fail"] = "Login Fail!";
-                    return RedirectToAction("Index");
-                }
+                TempData["fail"] = "Login Fail!";
+                return RedirectToAction("Index");
+            }
 
-                SqlConnection conn = new(_configuration.GetConnectionString("DbConnection"));
-                conn.Open();
-                string query = @"SELECT [UserId]
+            SqlConnection conn = new(_configuration.GetConnectionString("DbConnection"));
+            conn.Open();
+            string query = @"SELECT [UserId]
       ,[UserName]
       ,[PhoneNumber]
       ,[UserRole]
       ,[IsActive]
   FROM [dbo].[Users] WHERE PhoneNumber = @PhoneNumber AND Password = @Password AND IsActive = @IsActive";
-                SqlCommand cmd = new(query, conn);
-                cmd.Parameters.AddWithValue("@PhoneNumber", dataModel.PhoneNumber);
-                cmd.Parameters.AddWithValue("@Password", dataModel.Password);
-                cmd.Parameters.AddWithValue("@IsActive", true);
-                SqlDataAdapter adapter = new(cmd);
-                DataTable dt = new();
-                adapter.Fill(dt);
-                conn.Close();
+            SqlCommand cmd = new(query, conn);
+            cmd.Parameters.AddWithValue("@PhoneNumber", dataModel.PhoneNumber);
+            cmd.Parameters.AddWithValue("@Password", dataModel.Password);
+            cmd.Parameters.AddWithValue("@IsActive", true);
+            SqlDataAdapter adapter = new(cmd);
+            DataTable dt = new();
+            adapter.Fill(dt);
+            conn.Close();
 
-                if (dt.Rows.Count > 0)
-                {
-                    TempData["successMessage"] = "Login Successful!";
-                }
-                else
-                {
-                    TempData["fail"] = "Login Fail!";
-                }
-                return RedirectToAction("Index");
-            }
-            catch (Exception ex)
+            if (dt.Rows.Count > 0)
             {
-                throw new Exception(ex.Message);
+                TempData["successMessage"] = "Login Successful!";
             }
+            else
+            {
+                TempData["fail"] = "Login Fail!";
+            }
+            return RedirectToAction("Index");
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
         }
     }
 }
