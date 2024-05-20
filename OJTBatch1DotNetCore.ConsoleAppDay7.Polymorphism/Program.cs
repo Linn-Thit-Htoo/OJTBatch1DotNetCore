cbc7607a5e8f8030ccff7d5833863528a56d92a4
Linn Thit <lth1212001@gmail.com>
2024-05-20 23:17:08 +0630

    -

--- OJTBatch1DotNetCore.ConsoleAppDay7.Polymorphism/Program.cs
+++ OJTBatch1DotNetCore.ConsoleAppDay7.Polymorphism/Program.cs
@@ -1,26 +1,25 @@
-﻿namespace OJTBatch1DotNetCore.ConsoleAppDay7.Polymorphism
+﻿namespace OJTBatch1DotNetCore.ConsoleAppDay7.Polymorphism;
+
+public class Animal
 {
-    public class Animal
-    {
-        public virtual void MakeSound() => Console.WriteLine("Animal is making sound...");
-    }
+    public virtual void MakeSound() => Console.WriteLine("Animal is making sound...");
+}
 
-    public class Dog : Animal
-    {
-        public override void MakeSound() => Console.WriteLine("Dog is making sound...");
-    }
+public class Dog : Animal
+{
+    public override void MakeSound() => Console.WriteLine("Dog is making sound...");
+}
 
-    public class Cat : Animal
-    {
-        public override void MakeSound() => Console.WriteLine("Cat is making sound...");
-    }
+public class Cat : Animal
+{
+    public override void MakeSound() => Console.WriteLine("Cat is making sound...");
+}
 
-    public class Program
+public class Program
+{
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            Animal dog = new Dog();
-            dog.MakeSound();
-        }
+        Animal dog = new Dog();
+        dog.MakeSound();
     }
 }
