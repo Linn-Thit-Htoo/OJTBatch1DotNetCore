d28b578b704f7fbbc8ae2fc867109690ef370a9e
Linn Thit <lth1212001@gmail.com>
2024-05-20 23:21:05 +0630

    -

--- OJTBatch1DotNetCore.SampleApi.SampleMvc/Controllers/BlogController.cs
+++ OJTBatch1DotNetCore.SampleApi.SampleMvc/Controllers/BlogController.cs
@@ -2,34 +2,33 @@
 using Newtonsoft.Json;
 using OJTBatch1DotNetCore.SampleApi.SampleMvc.Models;
 
-namespace OJTBatch1DotNetCore.SampleApi.SampleMvc.Controllers
+namespace OJTBatch1DotNetCore.SampleApi.SampleMvc.Controllers;
+
+public class BlogController : Controller
 {
-    public class BlogController : Controller
+    private readonly HttpClient _httpClient;
+
+    public BlogController(HttpClient httpClient)
     {
-        private readonly HttpClient _httpClient;
+        _httpClient = httpClient;
+    }
 
-        public BlogController(HttpClient httpClient)
+    public async Task<IActionResult> Index()
+    {
+        try
         {
-            _httpClient = httpClient;
+            List<BlogDataModel> lst = new();
+            HttpResponseMessage response = await _httpClient.GetAsync("/api/Blog");
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = await response.Content.ReadAsStringAsync();
+                lst = JsonConvert.DeserializeObject<List<BlogDataModel>>(jsonStr)!;
+            }
+            return View(lst);
         }
-
-        public async Task<IActionResult> Index()
+        catch (Exception ex)
         {
-            try
-            {
-                List<BlogDataModel> lst = new();
-                HttpResponseMessage response = await _httpClient.GetAsync("/api/Blog");
-                if (response.IsSuccessStatusCode)
-                {
-                    string jsonStr = await response.Content.ReadAsStringAsync();
-                    lst = JsonConvert.DeserializeObject<List<BlogDataModel>>(jsonStr)!;
-                }
-                return View(lst);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            throw new Exception(ex.Message);
         }
     }
 }
