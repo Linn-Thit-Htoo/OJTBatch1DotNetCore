498d41356a00b7867b53c4bf05dc806bd28eb4a8
Linn Thit <lth1212001@gmail.com>
2024-05-21 17:51:46 +0630

    -

--- RentalApp/Form1.cs
+++ RentalApp/Form1.cs
@@ -1,32 +1,32 @@
 using System.Data;
 using System.Data.SqlClient;
 
-namespace RentalApp
+namespace RentalApp;
+
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    public Form1()
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
 
-        private void btnLogin_Click(object sender, EventArgs e)
+    private void btnLogin_Click(object sender, EventArgs e)
+    {
+        try
         {
-            try
+            string phoneNumber = txtPhoneNumber.Text;
+            string password = txtPassword.Text;
+
+            if (string.IsNullOrEmpty(phoneNumber) || string.IsNullOrEmpty(password))
             {
-                string phoneNumber = txtPhoneNumber.Text;
-                string password = txtPassword.Text;
+                MessageBox.Show("Please fill all fields...", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
 
-                if (string.IsNullOrEmpty(phoneNumber) || string.IsNullOrEmpty(password))
-                {
-                    MessageBox.Show("Please fill all fields...", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                    return;
-                }
+            SqlConnection conn = new(GetConnectionString._connStr);
+            conn.Open();
 
-                SqlConnection conn = new(GetConnectionString._connStr);
-                conn.Open();
-
-                string query = @"SELECT [UserId]
+            string query = @"SELECT [UserId]
       ,[UserName]
       ,[PhoneNumber]
       ,[Password]
@@ -35,44 +35,43 @@ namespace RentalApp
   FROM [dbo].[Users] WHERE PhoneNumber = @PhoneNumber AND Password = @Password AND IsActive = @IsActive
 AND UserRole = @UserRole";
 
-                SqlCommand cmd = new(query, conn);
-                cmd.Parameters.AddWithValue("@PhoneNumber", phoneNumber);
-                cmd.Parameters.AddWithValue("@Password", password);
-                cmd.Parameters.AddWithValue("@IsActive", true);
-                cmd.Parameters.AddWithValue("@UserRole", "admin");
+            SqlCommand cmd = new(query, conn);
+            cmd.Parameters.AddWithValue("@PhoneNumber", phoneNumber);
+            cmd.Parameters.AddWithValue("@Password", password);
+            cmd.Parameters.AddWithValue("@IsActive", true);
+            cmd.Parameters.AddWithValue("@UserRole", "admin");
 
-                SqlDataAdapter adapter = new(cmd);
-                DataTable dt = new();
-                adapter.Fill(dt);
+            SqlDataAdapter adapter = new(cmd);
+            DataTable dt = new();
+            adapter.Fill(dt);
 
-                conn.Close();
+            conn.Close();
 
-                if (dt.Rows.Count > 0)
-                {
-                    DialogResult result = MessageBox.Show("Login Successful!", "Information!", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    if (result == DialogResult.OK)
-                    {
-                        DashboardForm dashboardForm = new();
-                        dashboardForm.Show();
-                        this.Hide();
-                    }
-                }
-                else
-                {
-                    MessageBox.Show("Login Fail!", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    txtPhoneNumber.Text = string.Empty;
-                    txtPassword.Text = "";
-                }
-            }
-            catch (Exception ex)
+            if (dt.Rows.Count > 0)
             {
-                throw new Exception(ex.Message);
+                DialogResult result = MessageBox.Show("Login Successful!", "Information!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (result == DialogResult.OK)
+                {
+                    DashboardForm dashboardForm = new();
+                    dashboardForm.Show();
+                    this.Hide();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Login Fail!", "Warning!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtPhoneNumber.Text = string.Empty;
+                txtPassword.Text = "";
             }
         }
-
-        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        catch (Exception ex)
         {
-            Application.Exit();
+            throw new Exception(ex.Message);
         }
+    }
+
+    private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+    {
+        Application.Exit();
     }
 }
