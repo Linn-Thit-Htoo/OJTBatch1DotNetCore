ad51cdb3ce11f41bd096be0c0f8bfc5e32ea9447
Linn Thit <lth1212001@gmail.com>
2024-05-21 17:49:15 +0630

    -

--- ExpenseTrackerApi/Queries/IncomeCategoryQuery.cs
+++ ExpenseTrackerApi/Queries/IncomeCategoryQuery.cs
@@ -1,56 +1,55 @@
-﻿namespace ExpenseTrackerApi.Queries
+﻿namespace ExpenseTrackerApi.Queries;
+
+public static class IncomeCategoryQuery
 {
-    public static class IncomeCategoryQuery
+    public static string GetIncomeCategoryListQuery()
     {
-        public static string GetIncomeCategoryListQuery()
-        {
-            return @"SELECT [IncomeCategoryId]
+        return @"SELECT [IncomeCategoryId]
       ,[IncomeCategoryName]
       ,[IsActive]
   FROM [dbo].[Rest_Income_Category] WHERE IsActive = @IsActive ORDER BY IncomeCategoryId DESC";
-        }
+    }
 
-        public static string CreateIncomeCategoryQuery()
-        {
-            return @"INSERT INTO Rest_Income_Category (IncomeCategoryName, IsActive)
+    public static string CreateIncomeCategoryQuery()
+    {
+        return @"INSERT INTO Rest_Income_Category (IncomeCategoryName, IsActive)
 VALUES (@IncomeCategoryName, @IsActive)";
-        }
+    }
 
-        public static string CheckCreateIncomeCategoryDuplicateQuery()
-        {
-            return @"SELECT [IncomeCategoryId]
+    public static string CheckCreateIncomeCategoryDuplicateQuery()
+    {
+        return @"SELECT [IncomeCategoryId]
       ,[IncomeCategoryName]
       ,[IsActive]
   FROM [dbo].[Rest_Income_Category] WHERE IncomeCategoryName = @IncomeCategoryName AND IsActive = @IsActive";
-        }
+    }
 
-        public static string CheckIncomeCategoryDuplicateQuery()
-        {
-            return @"SELECT [IncomeCategoryId]
+    public static string CheckIncomeCategoryDuplicateQuery()
+    {
+        return @"SELECT [IncomeCategoryId]
       ,[IncomeCategoryName]
       ,[IsActive]
             FROM[dbo].[Rest_Income_Category] WHERE IncomeCategoryName = @IncomeCategoryName AND
           IsActive = @IsActive AND IncomeCategoryId != @IncomeCategoryId";
-        }
+    }
 
-        public static string UpdateIncomeCategoryQuery()
-        {
-            return @"UPDATE Rest_Income_Category SET IncomeCategoryName = @IncomeCategoryName 
+    public static string UpdateIncomeCategoryQuery()
+    {
+        return @"UPDATE Rest_Income_Category SET IncomeCategoryName = @IncomeCategoryName 
 WHERE IncomeCategoryId = @IncomeCategoryId";
-        }
+    }
 
-        public static string CheckIncomeCategoryExistsQuery()
-        {
-            return @"SELECT [IncomeId]
+    public static string CheckIncomeCategoryExistsQuery()
+    {
+        return @"SELECT [IncomeId]
       ,[IncomeCategoryId]
       ,[Amount]
       ,[IsActive]
   FROM [dbo].[Rest_Income] WHERE IncomeCategoryId = @IncomeCategoryId";
-        }
+    }
 
-        public static string DeleteIncomeCategoryQuery()
-        {
-            return @"UPDATE Rest_Income_Category SET IsActive = @IsActive WHERE IncomeCategoryId = @IncomeCategoryId";
-        }
+    public static string DeleteIncomeCategoryQuery()
+    {
+        return @"UPDATE Rest_Income_Category SET IsActive = @IsActive WHERE IncomeCategoryId = @IncomeCategoryId";
     }
 }
