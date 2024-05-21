7dda5255319d1b69123d965b757d88acfe3853bd
Linn Thit <lth1212001@gmail.com>
2024-05-21 17:52:29 +0630

    -

--- RentalApp/GetData.cs
+++ RentalApp/GetData.cs
@@ -1,30 +1,29 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
 
-namespace RentalApp
-{
-    public class GetData
-    {
-        public static DataTable Fetch(string tableName)
-        {
-            try
-            {
-                SqlConnection conn = new(GetConnectionString._connStr);
-                conn.Open();
-                string query = $"SELECT * FROM {tableName} WHERE IsActive = @IsActive";
-                SqlCommand cmd = new(query, conn);
-                cmd.Parameters.AddWithValue("@IsActive", true);
-                SqlDataAdapter adapter = new(cmd);
-                DataTable dt = new();
-                adapter.Fill(dt);
-                conn.Close();
+namespace RentalApp;
 
-                return dt;
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+public class GetData
+{
+    public static DataTable Fetch(string tableName)
+    {
+        try
+        {
+            SqlConnection conn = new(GetConnectionString._connStr);
+            conn.Open();
+            string query = $"SELECT * FROM {tableName} WHERE IsActive = @IsActive";
+            SqlCommand cmd = new(query, conn);
+            cmd.Parameters.AddWithValue("@IsActive", true);
+            SqlDataAdapter adapter = new(cmd);
+            DataTable dt = new();
+            adapter.Fill(dt);
+            conn.Close();
+
+            return dt;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
         }
     }
 }
