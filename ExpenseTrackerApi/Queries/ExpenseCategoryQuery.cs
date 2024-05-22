bd2ed4c029ee4856ef0775e186508e48950f6b40
Linn Thit <lth1212001@gmail.com>
2024-05-22 22:43:07 +0630

    -

--- ExpenseTrackerApi/Queries/ExpenseCategoryQuery.cs
+++ ExpenseTrackerApi/Queries/ExpenseCategoryQuery.cs
@@ -4,7 +4,6 @@ public static class ExpenseCategoryQuery
 {
     #region Get Expens eCategory List Query
 
-    #endregion
     public static string GetExpenseCategoryListQuery()
     {
         return @"SELECT [ExpenseCategoryId]
@@ -12,6 +11,7 @@ public static class ExpenseCategoryQuery
       ,[IsActive]
   FROM [dbo].[Rest_Expense_Category] WHERE IsActive = @IsActive ORDER BY ExpenseCategoryId DESC";
     }
+    #endregion
 
     public static string CheckCreateExpenseCategoryDuplicateQuery()
     {
