97370039385d9a1dcbd407234479afedda970b30
Linn Thit <lth1212001@gmail.com>
2024-05-22 22:43:00 +0630

    -

--- ExpenseTrackerApi/Queries/ExpenseCategoryQuery.cs
+++ ExpenseTrackerApi/Queries/ExpenseCategoryQuery.cs
@@ -2,7 +2,7 @@
 
 public static class ExpenseCategoryQuery
 {
-    #region MyRegion
+    #region Get Expens eCategory List Query
 
     #endregion
     public static string GetExpenseCategoryListQuery()
