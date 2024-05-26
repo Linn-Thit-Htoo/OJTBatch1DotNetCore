10645af944df598227117ef4a36164bba8dda9ff
Linn Thit <lth1212001@gmail.com>
2024-05-26 17:29:41 +0630

    -

--- ExpenseTrackerApi/Queries/ExpenseQuery.cs
+++ ExpenseTrackerApi/Queries/ExpenseQuery.cs
@@ -4,7 +4,6 @@ public static class ExpenseQuery
 {
     #region Get Expense List Query
 
-    #endregion
     public static string GetExpenseListQuery()
     {
         return @"SELECT ExpenseId, Rest_Expense_Category.ExpenseCategoryName, Rest_Users.UserName, Amount, Rest_Expense.IsActive,
@@ -15,6 +14,7 @@ INNER JOIN Rest_Users ON Rest_Expense.UserId = Rest_Users.UserId
 WHERE Rest_Expense.IsActive = @IsActive
 ORDER BY ExpenseId DESC";
     }
+    #endregion
 
     public static string GetExpenseListByUserIdQuery()
     {
