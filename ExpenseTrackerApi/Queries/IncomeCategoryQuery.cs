184db7c855ef8af0e4935c20f925b9c8f76322fa
Linn Thit <lth1212001@gmail.com>
2024-05-26 17:32:32 +0630

    -

--- ExpenseTrackerApi/Queries/IncomeCategoryQuery.cs
+++ ExpenseTrackerApi/Queries/IncomeCategoryQuery.cs
@@ -11,6 +11,7 @@ public static class IncomeCategoryQuery
       ,[IsActive]
   FROM [dbo].[Rest_Income_Category] WHERE IsActive = @IsActive ORDER BY IncomeCategoryId DESC";
     }
+
     #endregion
 
     public static string CreateIncomeCategoryQuery()
