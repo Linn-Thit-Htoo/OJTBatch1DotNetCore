186018c0c514e2ac9ba58018a1fc68baaf6dd676
Linn Thit Htoo <lth1212001@gmail.com>
2024-05-31 23:55:28 +0630

    -

--- OJTBatch1DotNetCore.FibonacciExercise/Program.cs
+++ OJTBatch1DotNetCore.FibonacciExercise/Program.cs
@@ -1,29 +1,28 @@
-﻿namespace OJTBatch1DotNetCore.FibonacciExercise
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            //  0, 1, 1, 2, 3, 5
-            int firstNum = 0;
-            int secondNum = 1;
+﻿namespace OJTBatch1DotNetCore.FibonacciExercise;
 
-            for (int i = 1; i <= 10; i++)
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //  0, 1, 1, 2, 3, 5
+        int firstNum = 0;
+        int secondNum = 1;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            int sum = firstNum + secondNum; // 1, 2, 3, 5
+            if (i == 1)
             {
-                int sum = firstNum + secondNum; // 1, 2, 3, 5
-                if (i == 1)
-                {
-                    Console.Write($"{firstNum}, {secondNum}, {sum}, "); // 0, 1, 1
-                                                                        //Console.Write($"{firstNum}, {secondNum}, {sum} "); // 0, 1, 1
-                }
-                else
-                {
-                    Console.Write(i == 10 ? sum : sum + ", ");
-                    //Console.Write(", " + sum);
-                }
-                firstNum = secondNum;
-                secondNum = sum;
+                Console.Write($"{firstNum}, {secondNum}, {sum}, "); // 0, 1, 1
+                                                                    //Console.Write($"{firstNum}, {secondNum}, {sum} "); // 0, 1, 1
             }
+            else
+            {
+                Console.Write(i == 10 ? sum : sum + ", ");
+                //Console.Write(", " + sum);
+            }
+            firstNum = secondNum;
+            secondNum = sum;
         }
     }
 }
