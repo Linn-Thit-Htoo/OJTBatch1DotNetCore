8ca50e01d6a050b3826194dc13c34adfe516d99d
Linn Thit Htoo <lth1212001@gmail.com>
2024-05-31 23:59:11 +0630

    -

--- RentalWebApp/Models/Entities/CategoryDataModel.cs
+++ RentalWebApp/Models/Entities/CategoryDataModel.cs
@@ -1,9 +1,8 @@
-﻿namespace RentalWebApp.Models.Entities
+﻿namespace RentalWebApp.Models.Entities;
+
+public class CategoryDataModel
 {
-    public class CategoryDataModel
-    {
-        public long CategoryId { get; set; }
-        public string CategoryName { get; set; }
-        public bool IsActive { get; set; }
-    }
+    public long CategoryId { get; set; }
+    public string CategoryName { get; set; }
+    public bool IsActive { get; set; }
 }
