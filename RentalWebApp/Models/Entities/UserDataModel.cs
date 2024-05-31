a8bcc1beea7824a390f92abd2c511c8c0b5c8d7f
Linn Thit Htoo <lth1212001@gmail.com>
2024-05-31 23:59:34 +0630

    -

--- RentalWebApp/Models/Entities/UserDataModel.cs
+++ RentalWebApp/Models/Entities/UserDataModel.cs
@@ -4,9 +4,9 @@ public class UserDataModel
 {
     public long UserId { get; set; }
     public string? MemberId { get; set; }
-    public string UserName { get; set; }
-    public string PhoneNumber { get; set; }
-    public string Password { get; set; }
-    public string UserRole { get; set; }
+    public string UserName { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string UserRole { get; set; } = null!;
     public bool IsActive { get; set; }
 }
