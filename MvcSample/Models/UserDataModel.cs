d1db11880df372e86a6734be837a749466de39c5
Linn Thit Htoo <lth1212001@gmail.com>
2024-05-31 23:56:26 +0630

    -

--- MvcSample/Models/UserDataModel.cs
+++ MvcSample/Models/UserDataModel.cs
@@ -4,7 +4,7 @@ public class UserDataModel
 {
     public long UserId { get; set; }
     public string UserName { get; set; } = null!;
-    public string PhoneNumber { get; set; }
+    public string PhoneNumber { get; set; } = null!;
     public string? Password { get; set; }
     public string UserRole { get; set; }
     public bool IsActive { get; set; }
