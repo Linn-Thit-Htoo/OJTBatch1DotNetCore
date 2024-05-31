6819410de56f1b554ae649e064f36dd31130763a
Linn Thit Htoo <lth1212001@gmail.com>
2024-05-31 23:59:04 +0630

    -

--- RentalWebApp/Models/Entities/BorrowDataModel.cs
+++ RentalWebApp/Models/Entities/BorrowDataModel.cs
@@ -1,12 +1,11 @@
-﻿namespace RentalWebApp.Models.Entities
+﻿namespace RentalWebApp.Models.Entities;
+
+public class BorrowDataModel
 {
-    public class BorrowDataModel
-    {
-        public long BorrowId { get; set; }
-        public long UserId { get; set; }
-        public long AssetId { get; set; }
-        public DateTime BorrowDate { get; set; }
-        public string ReturnDate { get; set; }
-        public bool IsActive { get; set; }
-    }
+    public long BorrowId { get; set; }
+    public long UserId { get; set; }
+    public long AssetId { get; set; }
+    public DateTime BorrowDate { get; set; }
+    public string ReturnDate { get; set; }
+    public bool IsActive { get; set; }
 }
