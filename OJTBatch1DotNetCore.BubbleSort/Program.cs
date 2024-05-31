26bd3eaae6c64bd0e19c543cf053dd9d12dfb0b3
Linn Thit Htoo <lth1212001@gmail.com>
2024-05-31 23:55:08 +0630

    -

--- OJTBatch1DotNetCore.BubbleSort/Program.cs
+++ OJTBatch1DotNetCore.BubbleSort/Program.cs
@@ -1,33 +1,32 @@
-﻿namespace OJTBatch1DotNetCore.BubbleSort
+﻿namespace OJTBatch1DotNetCore.BubbleSort;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        int[] nums = { 4, 3, 2, 1 };
+
+        bool isSorted = false;
+        while (!isSorted)
         {
-            int[] nums = { 4, 3, 2, 1 };
-
-            bool isSorted = false;
-            while (!isSorted)
+            isSorted = true;
+            for (int i = 0; i < nums.Length - 1; i++)
             {
-                isSorted = true;
-                for (int i = 0; i < nums.Length - 1; i++)
-                {
-                    int previousNum = nums[i]; // 4
-                    int nextNum = nums[i + 1]; // 3
+                int previousNum = nums[i]; // 4
+                int nextNum = nums[i + 1]; // 3
 
-                    if (nextNum < previousNum)
-                    {
-                        nums[i] = nextNum;
-                        nums[i + 1] = previousNum;
-                        isSorted = false;
-                    }
+                if (nextNum < previousNum)
+                {
+                    nums[i] = nextNum;
+                    nums[i + 1] = previousNum;
+                    isSorted = false;
                 }
             }
+        }
 
-            foreach (var num in nums)
-            {
-                Console.WriteLine(num);
-            }
+        foreach (var num in nums)
+        {
+            Console.WriteLine(num);
         }
     }
 }
