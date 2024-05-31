f926836369a07c4c7690cd4fa9865959274a5bdc
Linn Thit Htoo <lth1212001@gmail.com>
2024-05-31 23:58:50 +0630

    -

--- RentalWebApp/Models/Entities/AssetDataModel.cs
+++ RentalWebApp/Models/Entities/AssetDataModel.cs
@@ -1,14 +1,13 @@
-﻿namespace RentalWebApp.Models.Entities
+﻿namespace RentalWebApp.Models.Entities;
+
+public class AssetDataModel
 {
-    public class AssetDataModel
-    {
-        public long AssetId { get; set; }
-        public long CategoryId { get; set; }
-        public string AssetCode { get; set; }
-        public string AssetName { get; set; }
-        public string AssetStatus { get; set; }
-        public int Quantity { get; set; }
-        public DateTime CreateDate { get; set; }
-        public bool IsActive { get; set; }
-    }
+    public long AssetId { get; set; }
+    public long CategoryId { get; set; }
+    public string AssetCode { get; set; }
+    public string AssetName { get; set; }
+    public string AssetStatus { get; set; }
+    public int Quantity { get; set; }
+    public DateTime CreateDate { get; set; }
+    public bool IsActive { get; set; }
 }
