f72a57341599b2ce9ac84771c9db4631b900f8ce
Linn Thit Htoo <lth1212001@gmail.com>
2024-05-31 23:59:48 +0630

    -

--- RentalWebApp/Models/RequestModels/Asset/UpdateAssetRequestModel.cs
+++ RentalWebApp/Models/RequestModels/Asset/UpdateAssetRequestModel.cs
@@ -4,8 +4,8 @@
     {
         public long AssetId { get; set; }
         public long CategoryId { get; set; }
-        public string AssetCode { get; set; }
-        public string AssetName { get; set; }
+        public string AssetCode { get; set; } = null!;
+        public string AssetName { get; set; } = null!;
         public string AssetStatus { get; set; }
         public int Quantity { get; set; }
     }
