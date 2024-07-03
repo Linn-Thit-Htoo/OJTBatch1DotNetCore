db5afa649d060b86543512f9da117431ce7e25b2
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-03 13:06:12 +0630

    Format Code

--- OJTBatch1DotNetCore.RestApiDemo/Controllers/UserController.cs
+++ OJTBatch1DotNetCore.RestApiDemo/Controllers/UserController.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Data;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using OJTBatch1DotNetCore.RestApiDemo.Models;
 using OJTBatch1DotNetCore.RestApiDemo.Services;
-using System.Data;
-using System.Data.SqlClient;
 
 namespace OJTBatch1DotNetCore.RestApiDemo.Controllers;
 
@@ -33,35 +33,42 @@ public class UserController : ControllerBase
             if (string.IsNullOrEmpty(requestModel.Password))
                 return BadRequest("Password cannot be empty.");
 
-            string duplicateQuery = @"SELECT [UserId]
+            string duplicateQuery =
+                @"SELECT [UserId]
       ,[MemberId]
       ,[UserName]
       ,[PhoneNumber]
       ,[UserRole]
       ,[IsActive]
   FROM [dbo].[Users] WHERE PhoneNumber = @PhoneNumber AND IsActive = @IsActive";
-            List<SqlParameter> duplicateParams = new()
-            {
-                new SqlParameter("@PhoneNumber", requestModel.PhoneNumber),
-                new SqlParameter("@IsActive", true)
-            };
-            DataTable dt = _adoDotNetService.QueryFirstOrDefault(duplicateQuery, duplicateParams.ToArray());
+            List<SqlParameter> duplicateParams =
+                new()
+                {
+                    new SqlParameter("@PhoneNumber", requestModel.PhoneNumber),
+                    new SqlParameter("@IsActive", true)
+                };
+            DataTable dt = _adoDotNetService.QueryFirstOrDefault(
+                duplicateQuery,
+                duplicateParams.ToArray()
+            );
 
             if (dt.Rows.Count > 0)
             {
                 return Conflict("User with this phone number already exists!");
             }
 
-            string query = @"INSERT INTO Users (UserName, PhoneNumber, Password, UserRole, IsActive)
+            string query =
+                @"INSERT INTO Users (UserName, PhoneNumber, Password, UserRole, IsActive)
 VALUES (@UserName, @PhoneNumber, @Password, @UserRole, @IsActive)";
-            List<SqlParameter> parameters = new()
-            {
-                new SqlParameter("@UserName", requestModel.UserName),
-                new SqlParameter("@PhoneNumber", requestModel.PhoneNumber),
-                new SqlParameter("@Password", requestModel.Password),
-                new SqlParameter("@UserRole", "member"),
-                new SqlParameter("@IsActive", true)
-            };
+            List<SqlParameter> parameters =
+                new()
+                {
+                    new SqlParameter("@UserName", requestModel.UserName),
+                    new SqlParameter("@PhoneNumber", requestModel.PhoneNumber),
+                    new SqlParameter("@Password", requestModel.Password),
+                    new SqlParameter("@UserRole", "member"),
+                    new SqlParameter("@IsActive", true)
+                };
             int result = _adoDotNetService.Execute(query, parameters.ToArray());
 
             return result > 0 ? StatusCode(201, "Registration Successful!") : BadRequest("Fail!");
@@ -84,19 +91,21 @@ VALUES (@UserName, @PhoneNumber, @Password, @UserRole, @IsActive)";
             if (string.IsNullOrEmpty(requestModel.Password))
                 return BadRequest("Password cannot be empty.");
 
-            string query = @"SELECT [UserId]
+            string query =
+                @"SELECT [UserId]
       ,[MemberId]
       ,[UserName]
       ,[PhoneNumber]
       ,[UserRole]
       ,[IsActive]
   FROM [dbo].[Users] WHERE PhoneNumber = @PhoneNumber AND IsActive = @IsActive AND Password = @Password";
-            List<SqlParameter> parameters = new()
-            {
-                new SqlParameter("@PhoneNumber", requestModel.PhoneNumber),
-                new SqlParameter("@Password", requestModel.Password),
-                new SqlParameter("@IsActive", true),
-            };
+            List<SqlParameter> parameters =
+                new()
+                {
+                    new SqlParameter("@PhoneNumber", requestModel.PhoneNumber),
+                    new SqlParameter("@Password", requestModel.Password),
+                    new SqlParameter("@IsActive", true),
+                };
             DataTable user = _adoDotNetService.QueryFirstOrDefault(query, parameters.ToArray());
 
             if (user.Rows.Count == 0)
