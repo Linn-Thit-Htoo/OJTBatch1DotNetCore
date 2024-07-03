87083d8fe270b5894a7ce5d09f2fba3f7455d2f7
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-03 13:04:21 +0630

    Format Code

--- ExpenseTrackerApi/Models/RequestModels/User/RegisterRequestModel.cs
+++ ExpenseTrackerApi/Models/RequestModels/User/RegisterRequestModel.cs
@@ -5,8 +5,10 @@ namespace ExpenseTrackerApi.Models.RequestModels.User;
 public class RegisterRequestModel
 {
     public string UserName { get; set; } = null!;
+
     [EmailAddress]
     public string Email { get; set; } = null!;
+
     [MaxLength(12)]
     [MinLength(6)]
     public string Password { get; set; } = null!;
