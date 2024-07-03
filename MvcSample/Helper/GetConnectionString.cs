518ebde021db50140a12e52ffd9742de3d74eb94
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-03 13:00:42 +0630

    Format Code

--- MvcSample/Helper/GetConnectionString.cs
+++ MvcSample/Helper/GetConnectionString.cs
@@ -2,5 +2,6 @@
 
 public class GetConnectionString
 {
-    public static string _connStr = "Data Source=(local);Initial Catalog=OJT-Batch1;Integrated Security=True";
+    public static string _connStr =
+        "Data Source=(local);Initial Catalog=OJT-Batch1;Integrated Security=True";
 }
