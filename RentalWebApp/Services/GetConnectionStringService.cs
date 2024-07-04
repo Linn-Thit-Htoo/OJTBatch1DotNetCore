79dd2a27a481609e59f1b68b490032715324348b
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-04 12:28:11 +0630

    Format Code

--- RentalWebApp/Services/GetConnectionStringService.cs
+++ RentalWebApp/Services/GetConnectionStringService.cs
@@ -2,5 +2,6 @@
 
 public class GetConnectionStringService
 {
-    public static string _connStr = "Data Source=(local);Initial Catalog=OJT-Batch1;Integrated Security=True;TrustServerCertificate=True";
+    public static string _connStr =
+        "Data Source=(local);Initial Catalog=OJT-Batch1;Integrated Security=True;TrustServerCertificate=True";
 }
