c42ebe7bc70ef9e50e08bf509b90f5a8fd0790c5
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-04 12:27:01 +0630

    Format Code

--- RentalWebApp/Models/RequestModels/User/LoginRequestModel.cs
+++ RentalWebApp/Models/RequestModels/User/LoginRequestModel.cs
@@ -1,8 +1,7 @@
-﻿namespace RentalWebApp.Models.RequestModels.User
+﻿namespace RentalWebApp.Models.RequestModels.User;
+
+public class LoginRequestModel
 {
-    public class LoginRequestModel
-    {
-        public string PhoneNumber { get; set; }
-        public string Password { get; set; }
-    }
+    public string PhoneNumber { get; set; }
+    public string Password { get; set; }
 }
