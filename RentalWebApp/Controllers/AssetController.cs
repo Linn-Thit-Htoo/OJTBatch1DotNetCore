59ec4af8a6a7f51ad8388da9eb299421a99f172f
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-04 12:25:10 +0630

    Format Code

--- RentalWebApp/Controllers/AssetController.cs
+++ RentalWebApp/Controllers/AssetController.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Data;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using RentalWebApp.Models.Entities;
 using RentalWebApp.Models.RequestModels.Asset;
 using RentalWebApp.Models.ResponseModels;
 using RentalWebApp.Services;
-using System.Data;
-using System.Data.SqlClient;
 
 namespace RentalWebApp.Controllers;
 
@@ -21,14 +21,20 @@ public class AssetController : Controller
                 return RedirectToAction("LoginPage", "User");
             }
 
-            string query = @"SELECT AssetId, AssetCode, Category.CategoryName, Quantity, AssetName, AssetStatus, CreateDate, Asset.IsActive
+            string query =
+                @"SELECT AssetId, AssetCode, Category.CategoryName, Quantity, AssetName, AssetStatus, CreateDate, Asset.IsActive
 FROM Asset
 INNER JOIN Category ON Asset.CategoryId = Category.CategoryId
 WHERE Asset.IsActive = @IsActive AND Category.IsActive = @IsActive";
-            DataTable dt = DbHelper.Query(query, sqlParameters: new SqlParameter("@IsActive", true));
+            DataTable dt = DbHelper.Query(
+                query,
+                sqlParameters: new SqlParameter("@IsActive", true)
+            );
 
             string jsonStr = JsonConvert.SerializeObject(dt);
-            List<AssetResponseModel> lst = JsonConvert.DeserializeObject<List<AssetResponseModel>>(jsonStr)!;
+            List<AssetResponseModel> lst = JsonConvert.DeserializeObject<List<AssetResponseModel>>(
+                jsonStr
+            )!;
 
             return View(lst);
         }
@@ -48,14 +54,17 @@ WHERE Asset.IsActive = @IsActive AND Category.IsActive = @IsActive";
                 return RedirectToAction("LoginPage", "User");
             }
 
-            string query = @"SELECT [CategoryId]
+            string query =
+                @"SELECT [CategoryId]
       ,[CategoryName]
       ,[IsActive]
   FROM [dbo].[Category] WHERE IsActive = @IsActive";
             DataTable dt = DbHelper.Query(query, new SqlParameter("@IsActive", true));
 
             string jsonStr = JsonConvert.SerializeObject(dt);
-            List<CategoryDataModel> lst = JsonConvert.DeserializeObject<List<CategoryDataModel>>(jsonStr)!;
+            List<CategoryDataModel> lst = JsonConvert.DeserializeObject<List<CategoryDataModel>>(
+                jsonStr
+            )!;
 
             return View(lst);
         }
@@ -70,7 +79,8 @@ WHERE Asset.IsActive = @IsActive AND Category.IsActive = @IsActive";
     {
         try
         {
-            string duplicateTestingQuery = @"SELECT [AssetId]
+            string duplicateTestingQuery =
+                @"SELECT [AssetId]
       ,[CategoryId]
       ,[AssetCode]
       ,[AssetName]
@@ -78,11 +88,8 @@ WHERE Asset.IsActive = @IsActive AND Category.IsActive = @IsActive";
       ,[CreateDate]
       ,[IsActive]
   FROM [dbo].[Asset] WHERE AssetCode = @AssetCode AND IsActive = @IsActive";
-            List<SqlParameter> sqlParameters = new()
-            {
-                new("@AssetCode", dataModel.AssetCode),
-                new("@IsActive", true)
-            };
+            List<SqlParameter> sqlParameters =
+                new() { new("@AssetCode", dataModel.AssetCode), new("@IsActive", true) };
             DataTable asset = DbHelper.Query(duplicateTestingQuery, sqlParameters.ToArray());
             if (asset.Rows.Count > 0)
             {
@@ -90,18 +97,20 @@ WHERE Asset.IsActive = @IsActive AND Category.IsActive = @IsActive";
                 return RedirectToAction("AssetManagement");
             }
 
-            string query = @"INSERT INTO Asset (AssetCode, CategoryId, AssetName, AssetStatus, Quantity, CreateDate, IsActive)
+            string query =
+                @"INSERT INTO Asset (AssetCode, CategoryId, AssetName, AssetStatus, Quantity, CreateDate, IsActive)
 VALUES (@AssetCode, @CategoryId, @AssetName, @AssetStatus, @Quantity, @CreateDate, @IsActive)";
-            List<SqlParameter> parameters = new()
-            {
-                new("@CategoryId", dataModel.CategoryId),
-                new("@AssetCode", dataModel.AssetCode),
-                new("@AssetName", dataModel.AssetName),
-                new("@AssetStatus", dataModel.AssetStatus),
-                new("@Quantity", dataModel.Quantity),
-                new("@CreateDate", DateTime.Now),
-                new("@IsActive", true)
-            };
+            List<SqlParameter> parameters =
+                new()
+                {
+                    new("@CategoryId", dataModel.CategoryId),
+                    new("@AssetCode", dataModel.AssetCode),
+                    new("@AssetName", dataModel.AssetName),
+                    new("@AssetStatus", dataModel.AssetStatus),
+                    new("@Quantity", dataModel.Quantity),
+                    new("@CreateDate", DateTime.Now),
+                    new("@IsActive", true)
+                };
 
             int result = DbHelper.Execute(query, parameters.ToArray());
 
@@ -131,7 +140,8 @@ VALUES (@AssetCode, @CategoryId, @AssetName, @AssetStatus, @Quantity, @CreateDat
                 return RedirectToAction("LoginPage", "User");
             }
 
-            string query = @"SELECT [AssetId]
+            string query =
+                @"SELECT [AssetId]
       ,[CategoryId]
       ,[AssetCode]
       ,[AssetName]
@@ -140,37 +150,33 @@ VALUES (@AssetCode, @CategoryId, @AssetName, @AssetStatus, @Quantity, @CreateDat
       ,[CreateDate]
       ,[IsActive]
   FROM [dbo].[Asset] WHERE AssetId = @AssetId AND IsActive = @IsActive";
-            List<SqlParameter> parameters = new()
-            {
-                new("@AssetId", id),
-                new("@IsActive", true)
-            };
+            List<SqlParameter> parameters = new() { new("@AssetId", id), new("@IsActive", true) };
             DataTable asset = DbHelper.Query(query, parameters.ToArray());
             string assetJson = JsonConvert.SerializeObject(asset);
-            AssetDataModel assetDataModel = new()
-            {
-                AssetId = Convert.ToInt64(asset.Rows[0]["AssetId"]),
-                CategoryId = Convert.ToInt64(asset.Rows[0]["CategoryId"]),
-                AssetCode = Convert.ToString(asset.Rows[0]["AssetCode"])!,
-                AssetName = Convert.ToString(asset.Rows[0]["AssetName"])!,
-                AssetStatus = Convert.ToString(asset.Rows[0]["AssetStatus"])!,
-                Quantity = Convert.ToInt32(asset.Rows[0]["Quantity"])
-            };
+            AssetDataModel assetDataModel =
+                new()
+                {
+                    AssetId = Convert.ToInt64(asset.Rows[0]["AssetId"]),
+                    CategoryId = Convert.ToInt64(asset.Rows[0]["CategoryId"]),
+                    AssetCode = Convert.ToString(asset.Rows[0]["AssetCode"])!,
+                    AssetName = Convert.ToString(asset.Rows[0]["AssetName"])!,
+                    AssetStatus = Convert.ToString(asset.Rows[0]["AssetStatus"])!,
+                    Quantity = Convert.ToInt32(asset.Rows[0]["Quantity"])
+                };
 
-
-            string query1 = @"SELECT [CategoryId]
+            string query1 =
+                @"SELECT [CategoryId]
       ,[CategoryName]
       ,[IsActive]
   FROM [dbo].[Category] WHERE IsActive = @IsActive";
             DataTable category = DbHelper.Query(query1, new SqlParameter("@IsActive", true));
             string categoryJson = JsonConvert.SerializeObject(category);
-            List<CategoryDataModel> categories = JsonConvert.DeserializeObject<List<CategoryDataModel>>(categoryJson)!;
+            List<CategoryDataModel> categories = JsonConvert.DeserializeObject<
+                List<CategoryDataModel>
+            >(categoryJson)!;
 
-            EditAssetResponseModel respModel = new()
-            {
-                Categories = categories,
-                AssetDataModel = assetDataModel
-            };
+            EditAssetResponseModel respModel =
+                new() { Categories = categories, AssetDataModel = assetDataModel };
 
             return View(respModel);
         }
@@ -185,7 +191,8 @@ VALUES (@AssetCode, @CategoryId, @AssetName, @AssetStatus, @Quantity, @CreateDat
     {
         try
         {
-            string duplicateTestingQuery = @"SELECT [AssetId]
+            string duplicateTestingQuery =
+                @"SELECT [AssetId]
       ,[CategoryId]
       ,[AssetCode]
       ,[AssetName]
@@ -193,12 +200,13 @@ VALUES (@AssetCode, @CategoryId, @AssetName, @AssetStatus, @Quantity, @CreateDat
       ,[CreateDate]
       ,[IsActive]
   FROM [dbo].[Asset] WHERE AssetCode = @AssetCode AND IsActive = @IsActive AND AssetId != @AssetId";
-            List<SqlParameter> sqlParameters = new()
-            {
-                new("@AssetId", requestModel.AssetId),
-                new("@AssetCode", requestModel.AssetCode),
-                new("@IsActive", true)
-            };
+            List<SqlParameter> sqlParameters =
+                new()
+                {
+                    new("@AssetId", requestModel.AssetId),
+                    new("@AssetCode", requestModel.AssetCode),
+                    new("@IsActive", true)
+                };
             DataTable asset = DbHelper.Query(duplicateTestingQuery, sqlParameters.ToArray());
             if (asset.Rows.Count > 0)
             {
@@ -206,18 +214,19 @@ VALUES (@AssetCode, @CategoryId, @AssetName, @AssetStatus, @Quantity, @CreateDat
                 return RedirectToAction("AssetManagement");
             }
 
-
-            string query = @"UPDATE Asset SET CategoryId = @CategoryId, AssetCode = @AssetCode, AssetName = @AssetName,
+            string query =
+                @"UPDATE Asset SET CategoryId = @CategoryId, AssetCode = @AssetCode, AssetName = @AssetName,
 AssetStatus = @AssetStatus, Quantity = @Quantity WHERE AssetId = @AssetId";
-            List<SqlParameter> parameters = new()
-            {
-                new("@CategoryId", requestModel.CategoryId),
-                new("@AssetCode", requestModel.AssetCode),
-                new("@AssetName", requestModel.AssetName),
-                new("@AssetStatus", requestModel.AssetStatus),
-                new("@Quantity", requestModel.Quantity),
-                new("@AssetId", requestModel.AssetId)
-            };
+            List<SqlParameter> parameters =
+                new()
+                {
+                    new("@CategoryId", requestModel.CategoryId),
+                    new("@AssetCode", requestModel.AssetCode),
+                    new("@AssetName", requestModel.AssetName),
+                    new("@AssetStatus", requestModel.AssetStatus),
+                    new("@Quantity", requestModel.Quantity),
+                    new("@AssetId", requestModel.AssetId)
+                };
             int result = DbHelper.Execute(query, parameters.ToArray());
 
             if (result > 0)
@@ -248,11 +257,7 @@ AssetStatus = @AssetStatus, Quantity = @Quantity WHERE AssetId = @AssetId";
             }
 
             string query = @"UPDATE Asset SET IsActive = @IsActive WHERE AssetId = @AssetId";
-            List<SqlParameter> parameters = new()
-            {
-                new("@AssetId", id),
-                new("@IsActive", false)
-            };
+            List<SqlParameter> parameters = new() { new("@AssetId", id), new("@IsActive", false) };
             int result = DbHelper.Execute(query, parameters.ToArray());
 
             if (result > 0)
