fa124452a8cdf0d6585f597a6be606a83d689e8b
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-04 12:27:29 +0630

    Format Code

--- RentalWebApp/Models/ResponseModels/BorrowResponseModel.cs
+++ RentalWebApp/Models/ResponseModels/BorrowResponseModel.cs
@@ -1,15 +1,14 @@
-﻿namespace RentalWebApp.Models.ResponseModels
+﻿namespace RentalWebApp.Models.ResponseModels;
+
+public class BorrowResponseModel
 {
-    public class BorrowResponseModel
-    {
-        public long BorrowId { get; set; }
-        public DateTime BorrowDate { get; set; }
-        public string ReturnDate { get; set; }
-        public string MemberId { get; set; }
-        public string UserName { get; set; }
-        public string AssetCode { get; set; }
-        public string AssetName { get; set; }
-        public string AssetStatus { get; set; }
-        public string CategoryName { get; set; }
-    }
+    public long BorrowId { get; set; }
+    public DateTime BorrowDate { get; set; }
+    public string ReturnDate { get; set; }
+    public string MemberId { get; set; }
+    public string UserName { get; set; }
+    public string AssetCode { get; set; }
+    public string AssetName { get; set; }
+    public string AssetStatus { get; set; }
+    public string CategoryName { get; set; }
 }
