01d95be0e7708457588c8caf4d5fba18384f8feb
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-04 12:27:55 +0630

    Format Code

--- RentalWebApp/Services/DbHelper.cs
+++ RentalWebApp/Services/DbHelper.cs
@@ -1,48 +1,47 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
 
-namespace RentalWebApp.Services
+namespace RentalWebApp.Services;
+
+public class DbHelper
 {
-    public class DbHelper
+    public static DataTable Query(string query, params SqlParameter[] sqlParameters)
     {
-        public static DataTable Query(string query, params SqlParameter[] sqlParameters)
+        try
         {
-            try
-            {
-                SqlConnection conn = new(GetConnectionStringService._connStr);
-                conn.Open();
-                SqlCommand cmd = new(query, conn);
-                cmd.Parameters.AddRange(sqlParameters);
-                SqlDataAdapter adapter = new(cmd);
-                DataTable dt = new();
-                adapter.Fill(dt);
-                conn.Close();
+            SqlConnection conn = new(GetConnectionStringService._connStr);
+            conn.Open();
+            SqlCommand cmd = new(query, conn);
+            cmd.Parameters.AddRange(sqlParameters);
+            SqlDataAdapter adapter = new(cmd);
+            DataTable dt = new();
+            adapter.Fill(dt);
+            conn.Close();
 
-                return dt;
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            return dt;
         }
-
-        public static int Execute(string query, params SqlParameter[] sqlParameters)
+        catch (Exception ex)
         {
-            try
-            {
-                SqlConnection conn = new(GetConnectionStringService._connStr);
-                conn.Open();
-                SqlCommand cmd = new(query, conn);
-                cmd.Parameters.AddRange(sqlParameters);
-                int result = cmd.ExecuteNonQuery();
-                conn.Close();
+            throw new Exception(ex.Message);
+        }
+    }
 
-                return result;
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+    public static int Execute(string query, params SqlParameter[] sqlParameters)
+    {
+        try
+        {
+            SqlConnection conn = new(GetConnectionStringService._connStr);
+            conn.Open();
+            SqlCommand cmd = new(query, conn);
+            cmd.Parameters.AddRange(sqlParameters);
+            int result = cmd.ExecuteNonQuery();
+            conn.Close();
+
+            return result;
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
         }
     }
 }
